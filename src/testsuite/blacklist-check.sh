39f51c2fed7474e0bf0d33a06de1a69b58f4b9b4
[anp/hsw] <sysop@880.ru>
2005-03-24 22:31:53 +0600

    Import savannah.gnu.org tarball: ip-sentinel-0.1.tar.bz2

---
+++ src/testsuite/blacklist-check.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+. ${srcdir}/testsuite/functions
+
+outfile_err=`mktemp /tmp/ip-sentinel.check.XXXXXX`
+outfile_out=`mktemp /tmp/ip-sentinel.check.XXXXXX`
+outfile_fd3=`mktemp /tmp/ip-sentinel.check.XXXXXX`
+trap "rm -f ${outfile_err} ${outfile_out} ${outfile_fd3}" EXIT
+
+execfile=./blacklist-check
+ipfile=${basefile}.ips
+
+test -e ${ipfile} || ipfile=${datadir}/blacklist-check.ips
+
+function execprog()
+{
+    "$@" ${execfile} $ipfile ${basefile}.{lst,res} >${outfile_out} 3>${outfile_fd3}
+}
+
+function verify()
+{
+    sed -e 's!^.*\(: (Re)reading blacklist\)!TIME\1!;
+	    s!\(de:ad:be:ef:0:\)[^[:blank:]]*!\1XX!g' ${outfile_out} |
+	diff -c - ${basefile}.out || exit 1
+    diff -c ${outfile_fd3} ${basefile}.fd3 || exit 1
+}
+
+file ${execfile} | grep -q 'statically linked' || {
+    exists ef       && { execprog ef 2>&1 | sed -e '1,2d'; } && verify
+    exists valgrind && execprog valgrind -q && verify
+}
+
+execprog && verify
